5f92fad094c52762a703eb73741fa47374f6ffb4
Albin Åberg <albin.aberg@outlook.com>
2017-01-03 19:48:20 +0100

    LocalStorage works now. Still no clue if receiving from PubNub works.
    Changing button text accordingly when button is clicked.
    
    TODO: Automatic change of Button text when lightStatus changes.
    Tried a lot of things nothing working so far. 
    
    Add more buttons when the basic functionality is working which is:
    Send, Receive, Automatic Receive
    
    Before presentation: Make div result hidden

--- web/OnClickJs.js
+++ web/OnClickJs.js
@@ -4,6 +4,28 @@
  * and open the template in the editor.
  */
 
-function onClickLight() {
-    alert("BUTTON DO")
+function onClickLight(clicked_id) {
+    var lightStatus = document.getElementById("result").innerHTML = localStorage.getItem("light");
+    console.log(lightStatus)
+    if (lightStatus != null) {
+        if (lightStatus = "offLight") {
+            //do send status change    
+            
+            //change button name with the context of the button as parameter
+            document.getElementById(clicked_id).textContent = "Light On";
+        } else if (lightStatus = "onLight") {
+            //do send status change    
+            
+            //change button name with the context of the button as parameter
+            document.getElementById(clicked_id).textContent = "Light Off";
+        }
+    }
+    else {
+        //Get status and set name accordingly, set localStorage light accordingly aswell
+        
+        console.log("ERROR: LightStatus = Null")
+    }
+    console.log(clicked_id);
+
 }
+
